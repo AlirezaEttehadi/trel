b72778c78430a60fc6ff48dfd25e3f66edeba64d
komeilmehranfar <komeil.mehranfar@gmail.com>
2020-10-14 18:53:33 +0330

    refactor coumns

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App(props) {
-  const [tasks, setTasks] = useState({
+  const [board, setBoard] = useState({
     columns: [
       {
         priority: 1,
@@ -27,61 +27,25 @@ function App(props) {
     ],
   });
 
-  const onDragOver = (ev) => ev.preventDefault();
-
-  const onDragStart = (ev, id) => {
-    ev.dataTransfer.setData("id", id);
-  };
-
-  const onDrop = (ev, category) => {
-    const id = ev.dataTransfer.getData("id");
-
-    setTasks({
-      completed: [...tasks.completed, ...tasks.notCompleted].filter((task) => {
-        if (category === "completed" || task.id !== id) return task;
-        return null;
-      }),
-      notCompleted: [...tasks.completed, ...tasks.notCompleted].filter(
-        (task) => {
-          if (category === "notCompleted" || task.id !== id) return task;
-          return null;
-        }
-      ),
-    });
-  };
-
   return (
     <div className="container">
-      <div
-        className="column"
-        onDragOver={(e) => onDragOver(e)}
-        onDrop={(e) => onDrop(e, "notCompleted")}
-      >
-        {tasks.notCompleted.map((task) => (
-          <span
-            draggable
-            key={task.id}
-            onDragStart={(e) => onDragStart(e, task.id)}
-          >
-            {task.task}
-          </span>
-        ))}
-      </div>
-      <div
-        className="column"
-        onDragOver={(e) => onDragOver(e)}
-        onDrop={(e) => onDrop(e, "completed")}
-      >
-        {tasks.completed.map((task) => (
-          <span
-            draggable
-            key={task.id}
-            onDragStart={(e) => onDragStart(e, task.id)}
-          >
-            {task.task}
-          </span>
-        ))}
-      </div>
+      {board.columns.map((column) => (
+        <div
+          className="column"
+          // onDragOver={(e) => onDragOver(e)}
+          // onDrop={(e) => onDrop(e, "notCompleted")}
+        >
+          {column.tasks.map((task) => (
+            <span
+              draggable
+              key={task.id}
+              // onDragStart={(e) => onDragStart(e, task.id)}
+            >
+              {task.task}
+            </span>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
