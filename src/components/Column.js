94cc271be50ba9839e68bb2eb6a186137af200b8
komeilmehranfar <komeil.mehranfar@gmail.com>
2020-10-15 19:07:55 +0330

    add droggable

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { Draggable, Droppable } from "react-beautiful-dnd";
 
-function Column({ name, tasks }) {
+function Column({ name, tasks, id }) {
   return (
-    <div className="column">
-      <div>{name}</div>
-      {tasks.map((task) => (
-        <span draggable key={task.id}>
-          {task.task}
-        </span>
-      ))}
-    </div>
+    <Droppable droppableId={`droppable-${id}`} type="PERSON">
+      {(provided, snapshot) => (
+        <div
+          className="column"
+          ref={provided.innerRef}
+          style={{ backgroundColor: snapshot.isDraggingOver ? "blue" : "grey" }}
+          {...provided.droppableProps}
+        >
+          <div>{name}</div>
+          {tasks.map((task, index) => (
+            <Draggable draggableId={`draggble-${task.id}`} index={index}>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                >
+                  <div className="task" key={task.id}>
+                    {task.task}
+                  </div>
+                </div>
+              )}
+            </Draggable>
+          ))}
+        </div>
+      )}
+    </Droppable>
   );
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,37 +1,82 @@
 import React, { useState } from "react";
+import { DragDropContext } from "react-beautiful-dnd";
+import { produce } from "immer";
+
 import Column from "../components/Column";
 
 function App(props) {
   const [board, setBoard] = useState({
     columns: [
       {
+        id: 1,
         priority: 1,
         name: "to do",
         tasks: [
           { id: 1, task: "Go gym", status: "notCompleted" },
-          { id: 3, task: "Go bookstore", status: "notCompleted" },
-          { id: 4, task: "Go shopping", status: "notCompleted" },
-          { id: 6, task: "Go pool", status: "notCompleted" },
+          { id: 2, task: "Go bookstore", status: "notCompleted" },
+          { id: 3, task: "Go shopping", status: "notCompleted" },
+          { id: 4, task: "Go pool", status: "notCompleted" },
         ],
       },
       {
+        id: 2,
         priority: 2,
         name: "doing",
         tasks: [
-          { id: 1, task: "Go gym", status: "notCompleted" },
-          { id: 3, task: "Go bookstore", status: "notCompleted" },
-          { id: 4, task: "Go shopping", status: "notCompleted" },
-          { id: 6, task: "Go pool", status: "notCompleted" },
+          { id: 5, task: "Go gym", status: "notCompleted" },
+          { id: 6, task: "Go bookstore", status: "notCompleted" },
+          { id: 7, task: "Go shopping", status: "notCompleted" },
+          { id: 8, task: "Go pool", status: "notCompleted" },
         ],
       },
     ],
   });
+  const onDragEnd = (data) => {
+    console.log(data);
+    const taskId = data.draggableId.split("-")[1];
+    const columnId = data.source.droppableId.split("-")[1];
+    const sourceColumnIndex = board.columns.findIndex(
+      (col) => col.id === Number(columnId)
+    );
+    const sourceTaskIndex = board.columns[sourceColumnIndex].tasks.findIndex(
+      (t) => t.id === Number(taskId)
+    );
+    const destinationColumnIndex = board.columns.findIndex(
+      (col) => col.id === Number(data.destination.droppableId.split("-")[1])
+    );
+    const destinationTaskIndex = data.destination.index;
+    const _board = produce(board, (draftState) => {
+      const selectedTask = draftState.columns[sourceColumnIndex].tasks.splice(
+        sourceTaskIndex,
+        1
+      )[0];
+      draftState.columns[destinationColumnIndex].tasks.splice(
+        destinationTaskIndex,
+        0,
+        selectedTask
+      );
+    });
 
+    console.log(_board);
+    setBoard(_board);
+  };
+
+  const a = {
+    c: {
+      d: {},
+    },
+  };
   return (
     <div className="container">
-      {board.columns.map((column) => (
-        <Column name={column.name} tasks={column.tasks}></Column>
-      ))}
+      <DragDropContext onDragEnd={onDragEnd}>
+        {board.columns.map((column) => (
+          <Column
+            id={column.id}
+            name={column.name}
+            tasks={column.tasks}
+          ></Column>
+        ))}
+      </DragDropContext>
     </div>
   );
 }
